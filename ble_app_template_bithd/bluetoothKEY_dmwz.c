6dbe660f2fee46dd28cd316f0912191133da4b5d
DESKTOP-LEHSID8\pange <wangnan3.1415926@163.com>
2018-03-30 14:57:38 +0800

    when update firmware,no timeout

--- ble_app_template_bithd/bluetoothKEY_dmwz.c
+++ ble_app_template_bithd/bluetoothKEY_dmwz.c
@@ -6,53 +6,17 @@ void BlueUPfirmware(void * p_event_data, uint16_t event_size)
 {
 	if(Ble_ConnnectFlag==BLE_connect)
 	{
-		if(Main_status!=Main_status_download)
-		{
-			app_timer_stop(Timeout3Sec_id);   
-			Timeout3Sec_StarFlag=TimeClose;
-		}
-		else
-		{
-		
-			if(Timeout3Sec_StarFlag!=TimeClose)
-			{}
-			else
+		  if(Timeout3Sec_StarFlag!=TimeClose)
 			{
-				app_timer_start(Timeout3Sec_id,T_update_TimeOut, NULL);
-				Timeout3Sec_StarFlag=TimeOpen;                         
+				app_timer_stop(Timeout3Sec_id);   
+			  Timeout3Sec_StarFlag=TimeClose;
 			}
 
-			 
-			 if(Apdu_len_blekey!=0)
+			if(Apdu_len_blekey!=0)
 			{
 				CmdSendUart(Apducmd_uart,APDU_blekey,Apdu_len_blekey);
 				Apdu_len_blekey=0;
-			}
-			 
-			if(touch_key==KEY_LongTouch)
-			{
-				app_timer_stop(Timeout3Sec_id);       
-				Timeout3Sec_StarFlag=TimeClose;
-				KEYwork_flag=0;
-				Main_status=Main_status_closeoled;
-				PowerOff_key();
-				touch_key=KEY_NO;
-				KeyWorkflag=1;
-			}
-			 else
-			 {
-				 if(Timeout3Sec_StarFlag==TimeOut)
-					 {
-						app_timer_stop(Timeout3Sec_id);         
-						Timeout3Sec_StarFlag=TimeClose;
-						KEYwork_flag=0;
-						 Main_status=Main_status_closeoled;
-				 		PowerOff_key();
-					 }
-					 else
-					 {}			 
-			 }
-		}	 
+			} 
 	} 
 	else
 	{
